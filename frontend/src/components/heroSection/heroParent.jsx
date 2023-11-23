ff4f0a14ca5e1d479f1d4512c4514764d2e812f4
Kaifur Rahman <rahmankaifur8@gmail.com>
2023-11-23 21:15:15 +0530

    hero section implemented

---
+++ frontend/src/components/heroSection/heroParent.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import HeroSection from "./heroSection";
+function HeroParent() {
+  return <HeroSection></HeroSection>;
+}
+export default HeroParent;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
 import NavbarParent from "../components/navbar/navbarParent";
 import FooterParent from "../components/footer/footerParent";
+
 const Homepage = () => {
   return (
     <>
       <NavbarParent></NavbarParent>
+      <Outlet></Outlet>
       <FooterParent></FooterParent>
     </>
   );
