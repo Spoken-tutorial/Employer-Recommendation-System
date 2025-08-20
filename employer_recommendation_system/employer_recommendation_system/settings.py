52d55456665055d774f04960fdeac08a995e3ecf
ankitamk14 <ankitamk@gmail.com>
2025-08-21 01:37:52 +0530

    removed unsupported hashers

--- employer_recommendation_system/employer_recommendation_system/settings.py
+++ employer_recommendation_system/employer_recommendation_system/settings.py
@@ -204,14 +204,14 @@ AUTHENTICATION_BACKENDS = (
 )
 
 PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
+    # 'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    # 'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
+    # 'django.contrib.auth.hashers.CryptPasswordHasher',
 )
 
 
